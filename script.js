cd8daf69c4713fc7edab47e7a2406e8de4fe9f00
christiannbal <christianbaldoza@rocketmail.com>
2025-02-12 01:14:58 -0800

    added music

--- script.js
+++ script.js
@@ -1,34 +1,41 @@
-// Get elements
-const yesButton = document.getElementById("yesButton");
-const proposalScreen = document.getElementById("proposalScreen");
-const celebrationScreen = document.getElementById("celebrationScreen");
-const heartsContainer = document.querySelector(".hearts-container");
+// Create an audio object for the music
+const backgroundMusic = new Audio("music/Sexyy Red \"U My Everything\" ft. Drake (Official Audio).mp3");
+backgroundMusic.loop = true;
 
-// When "Yes" is clicked, switch to the celebration screen
+// Function to fade in music & start at 15 seconds
+function fadeInMusic(audio) {
+    audio.currentTime = 15;
+    audio.volume = 0;
+    audio.play();
+
+    let volume = 0;
+    let fadeInterval = setInterval(function() {
+        if (volume < 1) {
+            volume += 0.05;
+            audio.volume = volume;
+        } else {
+            clearInterval(fadeInterval);
+        }
+    }, 200);
+}
+
+// Modify "Yes" button event to start music
 yesButton.addEventListener("click", function() {
-    proposalScreen.style.display = "none"; 
-    celebrationScreen.style.display = "block"; 
-    createFloatingHearts(); 
-    createSpinningText(); 
-});
-
-var noButton = document.getElementById("no");
-noButton.addEventListener("mouseover", function() {
-    var i = Math.floor(Math.random() * window.innerWidth - 100);
-    var j = Math.floor(Math.random() * window.innerHeight - 50);
-    noButton.style.position = "absolute";
-    noButton.style.left = i + "px";
-    noButton.style.top = j + "px";
+    proposalScreen.style.display = "none";
+    celebrationScreen.style.display = "block";
+    fadeInMusic(backgroundMusic);
+    createFloatingHearts();
+    createSpinningText();
 });
 
 // Function to create floating hearts
 function createFloatingHearts() {
-    for (let i = 0; i < 30; i++) { // Generate 30 hearts
+    for (let i = 0; i < 30; i++) {
         let heart = document.createElement("div");
         heart.classList.add("heart");
-        heart.innerHTML = "❤️"; // Red heart emoji
-        heart.style.left = Math.random() * 100 + "vw"; 
-        heart.style.animationDuration = Math.random() * 3 + 2 + "s"; 
+        heart.innerHTML = "❤️";
+        heart.style.left = Math.random() * 100 + "vw";
+        heart.style.animationDuration = Math.random() * 3 + 2 + "s";
         heartsContainer.appendChild(heart);
 
         setTimeout(() => {
@@ -37,18 +44,17 @@ function createFloatingHearts() {
     }
 }
 
-// Function to create spinning "I LOVE YOU" text randomly
+// Function to create spinning "I LOVE YOU" text
 function createSpinningText() {
-    for (let i = 0; i < 10; i++) { // Generate 10 spinning text elements
+    for (let i = 0; i < 10; i++) {
         let spinText = document.createElement("div");
         spinText.classList.add("spinning-text");
         spinText.innerHTML = "I LOVE YOU 💖";
-        spinText.style.left = Math.random() * 90 + "vw"; // Random position
-        spinText.style.top = Math.random() * 80 + "vh"; // Random height
-        spinText.style.animationDuration = Math.random() * 4 + 3 + "s"; // Random spin duration
+        spinText.style.left = Math.random() * 90 + "vw";
+        spinText.style.top = Math.random() * 80 + "vh";
+        spinText.style.animationDuration = Math.random() * 4 + 3 + "s";
         document.body.appendChild(spinText);
 
-        // Remove text after animation completes
         setTimeout(() => {
             spinText.remove();
         }, 7000);
