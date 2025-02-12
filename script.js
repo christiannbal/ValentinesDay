b93c472e247d6c721ddc992f1470789644be9ac0
Christian Baldoza <122347675+christiannbal@users.noreply.github.com>
2025-02-11 18:27:22 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+// Add event listener to the "Yes" button
+document.getElementById("yesButton").addEventListener("click", function() {
+    alert("I love you! See you soon again!");
+});
 
+// Add event listener to the "No" button
+var noButton = document.getElementById("no");
+noButton.addEventListener("mouseover", function() {
+    // Move the "No" button to a random position
+    var i = Math.floor(Math.random() * window.innerWidth);
+    var j = Math.floor(Math.random() * window.innerHeight);
+    noButton.style.position = "absolute";
+    noButton.style.left = i + "px";
+    noButton.style.top = j + "px";
+});
